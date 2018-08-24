302606b12a07aa6dd7eeb679fa949bca446e5dd7
ShinyaOKA <39749063+ShinyaOKA@users.noreply.github.com>
2018-08-24 09:07:36 +0900

    Update gen_random_key.py

--- gen_random_key.py
+++ gen_random_key.py
@@ -3,7 +3,7 @@ import random
 
 
 def gen_random_key (key_length):
-    return = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(KEY_LENGTH)])
+    return = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(key_length)])
 
 
 #gen_random_key(128)
